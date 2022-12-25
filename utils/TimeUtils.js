f6ca8534756eec5627fa821a8179ed9d2e234088
ma9pie <ma9pie.dev@gmail.com>
2022-12-25 20:34:05 +0900

    히스토리 TimeUtils 수정

--- utils/TimeUtils.js
+++ utils/TimeUtils.js
@@ -3,16 +3,16 @@ const TimeUtils = () => {};
 TimeUtils.getBeforeHours = (date) => {
   const now = new Date();
   const beforeDate = new Date(date);
+  const minuts = Math.floor((now - beforeDate) / 1000 / 60);
   const hours = Math.floor((now - beforeDate) / 1000 / 60 / 60);
   const days = Math.floor(hours / 24);
-
-  if (hours <= 24) {
+  if (hours < 1) {
+    return `${minuts}분 전`;
+  } else if (hours <= 24) {
     return `${hours}시간 전`;
   } else {
     return `${days}일 전`;
   }
-
-  return hours;
 };
 
 export default TimeUtils;

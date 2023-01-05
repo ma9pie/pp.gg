237a192daed847901f7f5672c47f8c43bc714d9f
ma9pie <ma9pie.dev@gmail.com>
2023-01-06 03:17:54 +0900

    배포 테스트

--- pages/test.js
+++ pages/test.js
@@ -133,7 +133,7 @@ export async function getServerSideProps(context) {
     //   });
 
     await axios
-      .get("https://ppgg.vercel.app/api/v1/history", {
+      .get("https://ppgg.vercel.app/api/history", {
         params: {},
       })
       .then((res) => {

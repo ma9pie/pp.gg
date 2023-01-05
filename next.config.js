7baa2a5efb551ffff4f1ff88a17b46e2dcd4bce5
ma9pie <ma9pie.dev@gmail.com>
2023-01-06 02:51:04 +0900

    배포 테스트

--- next.config.js
+++ next.config.js
@@ -6,50 +6,50 @@ const nextConfig = {
     domains: ["opgg-static.akamaized.net", "ddragon.leagueoflegends.com"],
   },
 
-  async rewrites() {
-    return [
-      {
-        destination: "https://ppgg.vercel.app/:path*",
-        source: "/:path*",
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        destination: "https://ppgg.vercel.app/api/v1/:path*",
-        source: "/api/v1/:path*",
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        destination: "http://localhost:3000/:path*",
-        source: "/:path*",
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       destination: "https://ppgg.vercel.app/:path*",
+  //       source: "/:path*",
+  //     },
+  //   ];
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       destination: "https://ppgg.vercel.app/api/v1/:path*",
+  //       source: "/api/v1/:path*",
+  //     },
+  //   ];
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       destination: "http://localhost:3000/:path*",
+  //       source: "/:path*",
+  //     },
+  //   ];
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;

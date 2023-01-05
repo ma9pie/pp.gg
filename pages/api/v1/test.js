a426db815f579a5616f25c4bf2f335e9d2f887b1
ma9pie <ma9pie.dev@gmail.com>
2023-01-06 03:27:26 +0900

    배포 테스트

--- pages/api/v1/test.js
+++ pages/api/v1/test.js
@@ -8,9 +8,17 @@ export default async function handler(req, res) {
   await dbConnect();
 
   switch (method) {
+    // case "GET":
+    //   try {
+    //     res.status(200).json({ message: "Hello from Next.js!" });
+    //   } catch (error) {
+    //     res.status(400).json({ success: false });
+    //   }
+    //   break;
     case "GET":
       try {
-        res.status(200).json({ message: "Hello from Next.js!" });
+        const history = await History.find(query).sort({ date: -1 }).lean();
+        res.status(200).json(history);
       } catch (error) {
         res.status(400).json({ success: false });
       }
